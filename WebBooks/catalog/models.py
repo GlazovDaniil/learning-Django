a61d990db92f1261fe2f294e4e435688e1d48f0e
GlazovDaniil <d.glazow2016@yandex.ru>
2023-12-04 20:36:07 +0300

    ver 0.1

---
+++ WebBooks/catalog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+
+class BookInstance(models.Model):
+    due_back = models.DateField()
+
+class Status(models.Model):
+    status = models.CharField()
+class Autor(models.Model):
+    name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField()
+
+class Meta:
+    ordering = ["-my_field_name"]
+def __str__ (self):
+    return self.field_name
+
+class Language(models.Model):
+    name = models.CharField(max_length=30)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+
+class Books(models.Model):
+    title = models.CharField(max_length=50)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    nummary = models.CharField()
+    inprint = models.CharField(max_length=30)
+    ISBN = models.BigIntegerField()
+    genre = models
