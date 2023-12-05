22a346b2cd43cbd2fe0ecee3aa1e4b86283ef19d
GlazovDaniil <d.glazow2016@yandex.ru>
2023-12-05 21:31:38 +0300

    ver 0.1.1

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -1,33 +1,109 @@
 from django.db import models
-
+from django.shortcuts import reverse
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200,
+                            help_text="Введите жанр книги",
+                            verbose_name="Жанр книги")
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=20,
+                            help_text="Введите язык книги",
+                            verbose_name="Язык книги")
+    def __str__(self):
+        return self.name
+
+class Autor(models.Model):
+    name = models.CharField(max_length=100,
+                            help_text="Введите имя автора",
+                            verbose_name="Имя автора")
+    last_name = models.CharField(max_length=100,
+                                 help_text="Введиите фамилию автра",
+                                 verbose_name="Фамилия автора")
+    date_of_birth = models.DateField(
+        help_text="Введите дату рождения",
+        verbose_name="Дата рождения",
+        null=True, blank=True
+    )
+    date_of_death = models.DateField(
+        help_text="Введите дату смерти",
+        verbose_name="Дата смерти",
+        null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.last_name
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=200,
+                              help_text="Введите название книги",
+                              verbose_name="Название книги")
+    genre = models.ForeignKey('Genre',
+                              on_delete=models.CASCADE,
+                              help_text="Выберите жанр книги",
+                              verbose_name="Жанр книги", null=True)
+    language = models.ForeignKey('Language',
+                                 on_delete=models.CASCADE,
+                                 help_text="Выберите язык книги",
+                                 verbose_name="Язык книги", null=True)
+    autor = models.ManyToManyField('Autor',
+                              help_text="Выберете автора книги",
+                              verbose_name="Автор книги")
+    summary = models.TextField(max_length=1000,
+                               help_text="Введите описание книги",
+                               verbose_name="Аннотация книги"
+                               )
+    isbn = models.CharField(max_length=13,
+                                  help_text="Должно содержать 13 символов",
+                                  verbose_name="ISBN книги"
+                                  )
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=20,
+                            help_text="Введите статус экземпляра книги",
+                            verbose_name="Статус экземпляра книги"
+                            )
+    def __str__(self):
+        return self.name
 
 
 class BookInstance(models.Model):
-    due_back = models.DateField()
+    book = models.ForeignKey('Books', on_delete=models.CASCADE, null=True)
+    inv_nom = models.CharField(max_length=20, null=True,
+                               help_text="Введите инвентарный номер экземпляра",
+                               verbose_name="Инвентарный номер")
+    imprint = models.CharField(max_length=200,
+                               help_text="Введите издательство и год выnуска",
+                               verbose_name="Издательство")
+    status = models.ForeignKey('Status', on_delete=models.CASCADE,
+                               null=True,
+                               help_text="Изменить состояние экземпляра",
+                               verbose_name="Cтaтyc экземпляра книги")
+    due_back = models.DateField(null=True, blank=True,
+                                help_text="Введите конец срока статуса",
+                                verbose_name="Дaтa окончания статуса")
+    def __str__(self):
+        return '%s %s %s' % (self.inv_nom, self.book, self.status)
 
-class Status(models.Model):
-    status = models.CharField()
-class Autor(models.Model):
-    name = models.CharField(max_length=30)
-    date_of_birth = models.DateField()
-    date_of_death = models.DateField()
 
-class Meta:
-    ordering = ["-my_field_name"]
-def __str__ (self):
-    return self.field_name
 
-class Language(models.Model):
-    name = models.CharField(max_length=30)
+"""
+#возвращает обсолютный URL-адрес для отображения отдельных записей модели на веб-сайте
+def get_absolute_url(self):
+    return reverse('model-detail-view', args=[str(self.id)])
+"""
 
-class Genre(models.Model):
-    name = models.CharField(max_length=30)
 
-class Books(models.Model):
-    title = models.CharField(max_length=50)
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
-    nummary = models.CharField()
-    inprint = models.CharField(max_length=30)
-    ISBN = models.BigIntegerField()
-    genre = models
+
+
+
--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from .models import Books
 # Create your views here.
 
 def index(request):
+    a_record = Books(name="Книгa о вкусной еде")
+
+    """
+    В этихфильтрах использован формат: field_name__match_type, где field_name- имя
+    поля, по которому фильтруются данные, а match_type - тип соответствия поля 
+    определенному критерию
+    
+    wild_books = Books.objects.filter(title__contains='дикиe')
+    """
+
+    a_record.save()
     return HttpResponse("Main page WebBook!")
