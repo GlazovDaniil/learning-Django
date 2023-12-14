a411c8a54d35ef610cb6d642ef1fe691d0a092c2
GlazovDaniil <d.glazow2016@yandex.ru>
2023-12-14 11:33:18 +0300

    ver 1.0.3

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,12 +78,16 @@ WSGI_APPLICATION = 'WebBooks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = ['default'].update(db_from_env)
+
+"""{
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
+
 
 
 # Password validation
